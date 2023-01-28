7d9ba9c29363373df5edff0b79a42e8571e349a3
s-sanskar <sanskarpok11@gmail.com>
2023-01-28 16:20:00 -0500

    Audit and use iptables instead of ufw

---
+++ Linux/audit.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+becho() {
+    local arg2=${2:-" "}  # Set default value for $2 to " "
+    printf "\n"
+    if [ "$arg2" = " " ]; then
+        # echo -e "$(tput setaf 1)$1$(tput sgr0)"
+        echo -e "$1"
+    else
+        echo -n "$(tput setaf 1)$1$(tput sgr0)"
+    fi
+}
+
+becho "Hostname:"
+cat /etc/hostname
+
+becho "Timezone:"
+ls -ln /etc/localtime
+
+becho "OS INFO:"
+cat /etc/*-release
+
+becho "Kernel Info:"
+uname -a
+
+
+becho "IP Config:"
+if command -v ifconfig &> /dev/null; then
+    ifconfig
+else
+    ip a
+fi
+
+becho "Displaying Listening TCP Connections:"
+netstat -aptln
+
+becho "Displaying Listening UDP Connections:"
+netstat -apuln
+
+becho "Viewing Hostname-to-IP Address Mappings:"
+cat /etc/hosts
+lsattr /etc/hosts
+
+becho "Viewing DNS Resolution Settings:"
+cat /etc/resolv.conf
+lsattr /etc/resolv.conf
+
+becho "Check 600 File Permission:"
+ls -la /etc/shadow
+lsattr /etc/shadow
+
+ls -la /etc/gshadow
+lsattr /etc/gshadow
+
+becho "Check 644 File Permission:"
+ls -la /etc/passwd
+lsattr /etc/passwd
+
+ls -la /etc/group
+lsattr /etc/group
+
+becho "Check 440 File Permission:"
+ls -la /etc/sudoers
+lsattr /etc/sudoers
+
+becho "Check non-root owned File in /etc"
+find /etc/ -not -user root
+
+becho "Current Status of /tmp:"
+ls -la /tmp
+lsattr /tmp
+
+becho "Current Status of /var/tmp:"
+ls -la /var/tmp
+lsattr /var/tmp
+
+becho "List current user (w)"
+w
+
+becho "Timeline of boot and recent access"
+who -a
+
+becho "IP addresses of the last several logons"
+last
+
+becho "Check crontab:"
+if command -v crontab &> /dev/null; then
+    for user in $(cut -f1 -d: /etc/passwd); do crontab -u $user -l; done
+fi
+
+
+becho "Sudoers Config:"
+cat /etc/sudoers /etc/sudoers.d/* | grep -v "^# " | sed '/^$/N;/^\n$/D'
+
+becho "Services (systemctl)"
+if command -v systemctl &> /dev/null; then
+    systemctl list-units --type=service
+fi
+
+becho "Services (service or /etc/init.d/*)"
+if command -v service &> /dev/null; then
+    service --status-all
+else
+    ls -l /etc/init.d/*
+fi
+
+becho "Users with id of 0"
+awk -F: '($3=="0"){print}' /etc/passwd
+
+becho "Users with .ssh files"
+find /home -type d -name ".ssh"
+
+becho "Amount of disk space available"
+df -h
+
+read -p "Press any key to start the backup process...."
+# Backup Dir
+mkdir -p /audit/backup
+chown -R root:root /audit
+chmod -R 600 /audit
+backup_dir="/audit/backup"
+
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+
+becho "Backup ps aux --forest"
+ps aux --forest > "$backup_dir/ps.aux.forest.$timestamp.txt"
+
+becho "Backup /usr/bin list (Not the binary)"
+ls -la /usr/bin/ > "$backup_dir/usr.bin.$timestamp.txt"
+
+becho "Backup /usr/sbin list (Not the binary)"
+ls -la /usr/sbin/ > "$backup_dir/usr.sbin.$timestamp.txt"
+
+becho "Backup /home list (Not the binary)"
+ls -la /home > "$backup_dir/home.$timestamp.txt"
+
+
+read -p "Press any key to backup /etc folder (pls check your storage)...."
+
+# To view the tar backup (untar): tar -xzf your_archive.tar.gz -C /path/to/destination_directory
+becho "Backup /etc"
+tar -czvf "$backup_dir/etc_backup_$timestamp.tar.gz" /etc
+
+chmod -R 600 /audit
--- Linux/firewall.sh
+++ Linux/firewall.sh
@@ -1,39 +1,110 @@
+#!/bin/bash
 
-set_up_firewall() {
-    # To list the rules of the current iptables
-    iptables -L --line-numbers | tee -a prev_iptable.log
-    # Install ufw
-    printf "Install ufw"
-    apt install ufw
-
-    printf "ufw status"
-    ufw status | tee -a ufw_status.log
-    
-    printf "Disable ufw"
-    ufw disable
-
-    printf "Reset all rules"
-    ufw reset
-
-    printf "Disable firewall"
-    systemctl stop ufw
-
-    printf "vim /etc/default/ufw"
-
-    ufw default deny incoming
-
-    ufw default allow outgoing
-
-    ufw allow 22
-    ufw allow 80
-    ufw allow 443
-
-    systemctl start ufw 
-
-    ufw enable
-
-    ufw status
+becho() {
+    local arg2=${2:-" "}  # Set default value for $2 to " "
+    printf "\n"
+    if [ "$arg2" = " " ]; then
+        # echo -e "$(tput setaf 1)$1$(tput sgr0)"
+        echo -e "$1"
+    else
+        echo -n "$(tput setaf 1)$1$(tput sgr0)"
+    fi
 }
 
-touch firewall_set_up.log
-set_up_firewall | tee -a firewall_set_up.log
+# Backup Dir
+mkdir -p /audit/backup
+chown -R root:root /audit
+chmod -R 600 /audit
+
+backup_dir="/audit/backup"
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+
+becho "Save Iptables:"
+if command -v iptables &> /dev/null; then
+    iptables-save > "$backup_dir/iptables.$timestamp.rules"
+else
+    becho "iptables Not Found"
+    exit 1
+fi
+
+becho "List IP Tables Rules"
+iptables -L --line-numbers
+
+
+printf "\n"
+read -p "Press any key to flush current iptables rules..."
+
+becho "Flush iptables"
+iptables -F
+
+becho "Allowing everything on loopback"
+iptables -A INPUT -s 127.0.0.1 -j ACCEPT
+iptables -A OUTPUT -d 127.0.0.1 -j ACCEPT
+
+becho "Dropping anything marked INVALID"
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+
+becho "Allow Ping"
+iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+becho "ACCEPT *incoming* RELATED/ESTABLISHED"
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+becho "ACCEPT *outgoing* RELATED/ESTABLISHED"
+iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+
+# Ask Questions for popular ports
+port=22
+echo "Allow incomming and outgoing for port $port (y/n) ?"
+read allowSSH
+if [ "$allowSSH" == "y" ]; then
+    iptables -A INPUT -p tcp --dport $port -m conntrack --ctstate NEW -j ACCEPT
+    iptables -A OUTPUT -p tcp --dport $port -m conntrack --ctstate NEW -j ACCEPT
+fi
+
+port=443
+echo "Allow incomming and outgoing for port $port (y/n) ?"
+read allowHTTPS
+if [ "$allowHTTPS" == "y" ]; then
+    iptables -A INPUT -p tcp --dport $port -m conntrack --ctstate NEW -j ACCEPT
+    iptables -A OUTPUT -p tcp --dport $port -m conntrack --ctstate NEW -j ACCEPT
+fi
+
+port=80
+echo "Allow incomming and outgoing for port $port (y/n) ?"
+read allowHTTP
+if [ "$allowHTTP" == "y" ]; then
+    iptables -A INPUT -p tcp --dport $port -m conntrack --ctstate NEW -j ACCEPT
+    iptables -A OUTPUT -p tcp --dport $port -m conntrack --ctstate NEW -j ACCEPT
+fi
+
+port=53
+echo "ACCEPT outgoing DNS [port 53/udp] (y/n) ?"
+read allowDNS
+if [ "$allowDNS" == "y" ]; then
+    iptables -A INPUT -p tcp --dport $port -m conntrack --ctstate NEW -j ACCEPT
+fi
+
+
+becho "Set Default Policy to DROP"
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
+iptables -P INPUT DROP
+
+becho "Graceful UDP REJECTs"
+iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+
+becho "Graceful TCP REJECTs"
+iptables -A INPUT -p tcp -j REJECT --reject-with tcp-rst
+
+becho "Graceful UNKNOWN REJECTs"
+iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+
+
+mkdir -p /etc/iptables
+
+becho "Save IP table at /etc/iptables/iptables.rules"
+iptables-save | tee /etc/iptables/iptables.rules
+chmod +x /etc/iptables/iptables.rules
